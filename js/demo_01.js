fdcedc8e2a9dbd71ceaf12b54bee584b9bb4f80a
Mac Duy Hai <macduy@gmail.com>
2012-10-28 00:38:42 +0100

    Accolades, Game completion.

--- js/demo_01.js
+++ js/demo_01.js
@@ -24,12 +24,25 @@ function reset() {
         .style('opacity', 0)
         .style('font-size', '0px')
         .style('-webkit-filter', 'blur(0px) brightness(0)')
+    ;
 
     // accolades
     d3.selectAll(".accolades img")
         .style('opacity', 0)
         .attr('width', 0)
-        .style('-webkit-filter', 'saturate(0)')
+        .style('-webkit-filter', 'brightness(-0.2) hue-rotate(0deg) saturate(0)')
+        .style('margin', '0px')
+    ;
+
+    d3.selectAll(".accolades div")
+        .style('opacity', 0)
+        .style('font-size', '0px')
+    ;
+
+    // links
+    d3.select(".links")
+        .style('height', '0px')
+    ;
 }
 
 // Animate - at the end of the animation, set finish to true
@@ -78,29 +91,54 @@ function animate() {
             // highlight and
             .transition().duration(100).delay(1300).ease('cubic-out')
                 .style('-webkit-filter', 'blur(1px) brightness(1)')
-                .style('font-size', '60px')
+                .style('font-size', '50px')
             .transition().duration(750).delay(1500).ease('cubic-in-out')
                 .style('-webkit-filter', 'blur(0px) brightness(0)')
-                .style('font-size', '40px')
+                .style('font-size', '36px')
         ;
     }
 
     function animate_accolades() {
-        return d3.selectAll('.accolades img')
-            .transition().duration(500).delay(function(d, i) { return i * 500; })
+        d3.selectAll('.accolades img')
+            .transition().duration(500).delay(function(d, i) { return i * 1100; })
                 .attr('width', 48)
                 .style('opacity', 1)
+                .style('margin', '5px')
+            .transition().duration(500).delay(function(d, i) { return i * 1100 + 500}).ease('cubic-out')
+                .style('-webkit-filter', function(d, i) { return 'brightness(0) hue-rotate(' + i * 72 + 'deg) saturate(2)' })
+        ;
+
+        return d3.selectAll(".accolades div")
+            .transition().duration(500).delay(function(d, i) { return i * 1100; })
+                .style('opacity', 1)
+                .style('font-size', '20px')
+            .transition().duration(500).delay(function(d, i) { return (i+1) * 1100 + 200; })
+                .style('opacity', 0)
+                .style('font-size', '0px')
             ;
     }
+
+    function animate_links() {
+        return d3.select('.links')
+            .transition().duration(600)
+                .style('height', '32px')
+        ;
+    }
+
+    var count = 0;
 
     animate_container().each('end', function() {
         animate_badge().each('end', function() {
             animate_score().each('end', function() {
                 animate_accolades().each('end', function() {
-                    // TODO: need to count all accolade elements before setting these
-                    // but this isn't such a big deal.
-                    finished = true;
-                    animating =false;
+                    // wait until each accolade is finished displaying
+                    count++;
+                    if (count == 5) {
+                        animate_links().each('end', function() {
+                            finished = true;
+                            animating =false;
+                        })
+                    }
                 });
             });
         });
